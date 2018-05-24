07edd65c7eb5134dd4ebd33253bb045d7402a967
The_Circuit <kristofferomdal@gmail.com>
2018-05-24 21:45:35 +0200

    formatting and minor changes

--- moduler/euler.py
+++ moduler/euler.py
@@ -12,23 +12,25 @@ DELTA_T = 1
 
 class Tank:
     def __init__(self,AA = 1,AS = 1,FH = 1,c=0.61):
-            self.AREA_APERTURE = AA #0.00001
-            self.AREA_SURFACE = AS #0.196*0.28
-            self.fluidHeight = FH #0.021
+            self.AREA_APERTURE = AA
+            self.AREA_SURFACE = AS
+            self.fluidHeight = FH
             self.K = c*((G*2)**0.5)
 
     def runStep(self,q_in):
-        self.fluidHeight -=(self.getHeightDiff(
-            self.fluidHeight,
-            self.AREA_APERTURE,
-            self.AREA_SURFACE)*DELTA_T
-            - (q_in/self.AREA_SURFACE)*DELTA_T)
+        #remove fluid that runs out of the tank
+        self.changeVolume(self.getVolumeDiff()*DELTA_T)
+        #add fluid that runs in to the tank
+        self.changeVolume(q_in*DELTA_T)
 
     def getFlowVelocity(self,H):
         return self.K*(H**0.5)
 
-    def getHeightDiff(self,H,AA,AS):
-        return self.getFlowVelocity(H)*(AA/AS)#*CONTRACTION_COEFFICIENT
+    def getVolumeDiff(self):
+        return -self.getFlowVelocity(self.fluidHeight)*self.AREA_APERTURE
+
+    def changeVolume(self,volume):
+        self.fluidHeight += volume/self.AREA_SURFACE
 
 
 if __name__ == '__main__':
